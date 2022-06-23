3ef239f04701d43c159388cb5572b21530ff639a
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-06-22 22:10:34 -0400

    Update sketch4.js
    Try a while loop again with noLoop().

--- sketch4.js
+++ sketch4.js
@@ -3,26 +3,29 @@ function setup() {
  cnv.position(50,200);
  x=0;
  y=0;  
-// noLoop();
+ noLoop();
 }
   function draw() {
     background(51);
     angleMode(DEGREES); // Change the mode to DEGREES
   //  clear();
+   let i=1;
+   while (i < 100) {
     x=120+2*rotationY;
     y=2*rotationX;
     if (x > 275) {
-      x=275
+      x=275;
      }if (y > 275) {
-      y=275
+      y=275;
      }
    if (x < 0) {
-      x=0
+      x=0;
      }if (y < 0) {
-      y=0
+      y=0;
      }
     erase();
     rect(x,y,25,25);
-    noErase();
+    i=i+1;
+   }
   }
   
