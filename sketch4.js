f6f726873b3f580c46c92c711f1c3a9aa49b2355
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-06-23 22:02:59 -0400

    Update sketch4.js
    Put background in setup to make squares not get erased? Oh, and also took out old, commented-out lines.

--- sketch4.js
+++ sketch4.js
@@ -3,14 +3,13 @@ function setup() {
  cnv.position(50,200);
  x=0;
  y=0;  
- noLoop();
+ background(51);
 }
   function draw() {
-    background(51);
+   
     angleMode(DEGREES); // Change the mode to DEGREES
-  //  clear();
-   let display = touches.length;
-   while (display < 1) {
+  
+  
     
     x=120+2*rotationY;
     y=2*rotationX;
@@ -24,9 +23,7 @@ function setup() {
      }if (y < 0) {
       y=0;
      }
-    erase();
+   
     rect(x,y,25,25);
-  //  i=i+1;
-   }
   }
   
