ddcb606076dbaa0989f5e004ba664ce8d0163c65
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-06-26 11:14:17 -0400

    Update sketch4.js
    Try the map() function and clean up extraneous stuff

--- sketch4.js
+++ sketch4.js
@@ -1,41 +1,31 @@
 function setup() {
- let cnv= createCanvas(300, 300);
- cnv.position(50,200);
- x=0;
- y=0;  
- background(51);
-}
-  function draw() {
-   
-     angleMode(DEGREES); // Change the mode to DEGREES
-//    let tutu= touches.length;
- //   if (tutu > 0) {
- //   noLoop();
- //   }
-  
-    
-   // x=120+2*(radians(rotationY));
-    x=120+2*(rotationY);
-    y=2*(rotationX);
-  //  y=2*(radians(rotationX));
-    if (x > 275) {
-      x=275;
-     }if (y > 275) {
-      y=275;
+    let cnv= createCanvas(300, 300);
+    cnv.position(50,200);
+    x=0;
+    y=0;  
+    background(51);
+   }
+     function draw() {
+      
+        angleMode(DEGREES); // Change the mode to DEGREES
+       x=map(rotationY, 0, 180, 0,300);
+       y=map(rotationX,0,180,0,300);
+       if (x > 275) {
+         x=275;
+        }if (y > 275) {
+         y=275;
+        }
+      if (x < 0) {
+         x=0;
+        }if (y < 0) {
+         y=0;
+        }
+      
+       rect(x,y,25,25);
      }
-   if (x < 0) {
-      x=0;
-     }if (y < 0) {
-      y=0;
+     function touchStarted() {
+     noLoop();
      }
-   
-    rect(x,y,25,25);
-  }
-  function touchStarted() {
-  noLoop();
-  }
-  function touchEnded() {
-   loop();
-  }
-   
-  
+     function touchEnded() {
+      loop();
+     }
