741ec74707613dd6492fd9437420527ac80dafbd
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-06-29 09:36:13 -0400

    Update scroll test.js
    Changed x-value boundary to 50.

--- scroll test.js
+++ scroll test.js
@@ -7,7 +7,7 @@ function setup() {
      function draw() {
    
        angleMode(DEGREES); // Change the mode to DEGREES
-       x=map(rotationY, -50, 50, 0, 200);
+       x=map(rotationY, -50, 50, 0, 50);
        y=map(rotationX, 0, 90, 3500, 0);
        window.scrollTo(x, y);          
      }
