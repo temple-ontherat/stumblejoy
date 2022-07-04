44db9bde588497dba64b9c2db4b70cd1e1a9445f
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-07-04 13:31:34 -0400

    Update scroll test.js
    Still trying to make x values (Y axis rotation) work. Changed initial map values back to real two-way 90 degree turns.

--- scroll test.js
+++ scroll test.js
@@ -8,7 +8,7 @@ function setup() {
        clear();
    
        angleMode(DEGREES); // Change the mode to DEGREES
-       let x=map(rotationY, 0, 50, 0, 100);
+       let x=map(rotationY, -90, 90, 0, 100);
        //y=map(rotationX, 0, 90, 3500, 0);
        window.scrollTo(x, 0);          
      }
