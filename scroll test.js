1f11d0f8994de5565f3e1146ff8ed5e7a0163821
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-07-04 13:38:12 -0400

    Update scroll test.js
    Let's make a x/y values display to see what x is doing.

--- scroll test.js
+++ scroll test.js
@@ -10,6 +10,8 @@ function setup() {
        angleMode(DEGREES); // Change the mode to DEGREES
        let x=map(rotationY, -90, 90, 0, 100);
        //y=map(rotationX, 0, 90, 3500, 0);
+       text(x, 10, 30);
+    text(y, 10, 60);
        window.scrollTo(x, 0);          
      }
     
