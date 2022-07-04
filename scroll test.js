d5c3f808976ca6a9b518df30b38e15bd7491cf1c
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-07-04 16:12:54 -0400

    Update scroll test.js
    Try changing x to whole number

--- scroll test.js
+++ scroll test.js
@@ -8,10 +8,13 @@ function setup() {
        clear();
    
        angleMode(DEGREES); // Change the mode to DEGREES
-       let x=map(rotationY, -10, 10, 0, 20);
+       let est=rotationY.toFixed(0)
+
+       let x=map(est, -10, 10, 0, 20);
+      
        //y=map(rotationX, 0, 90, 3500, 0);
        text(x, 10, 30);
-    text(y, 10, 60);
+   // text(y, 10, 60);
        window.scrollTo(x, 0);          
      }
     
