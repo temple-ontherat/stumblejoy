532eed31b98cb776a53392e92d1cf80fb055f4c1
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-07-04 22:02:56 -0400

    Update rotation coordinates.js
    corrected typo

--- rotation coordinates.js
+++ rotation coordinates.js
@@ -5,8 +5,8 @@ function setup() {
 function draw() {
     clear();
     
-    let x =map(rotationY, -90,90,0,200,true);
-    let y =map(rotationX; 0,90,0,200,true);
+    let x =map(rotationY, -90, 90, 0, 200, true);
+    let y =map(rotationX, 0, 90, 0, 200, true);
    
     text(x, 10, 30);
     text(y, 10, 60);
