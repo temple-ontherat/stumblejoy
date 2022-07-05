e50e3003f9c761e6b20e627e755367b2c5c2b09f
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-07-04 21:54:37 -0400

    Update rotation coordinates.js
    test map() values for adjusted rotation.

--- rotation coordinates.js
+++ rotation coordinates.js
@@ -5,8 +5,9 @@ function setup() {
 function draw() {
     clear();
     
-    let x = rotationY;
-    let y = rotationX;
+    let x =map(rotationY, -90,90,0,200,true);
+    let y =map(rotationX; 0,90,0,200,true);
+   
     text(x, 10, 30);
     text(y, 10, 60);
   }
