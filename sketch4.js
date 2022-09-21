6c7f386f92b4c48e4a1c6c6f6df7aabc08fb2e31
temple-ontherat <102549386+temple-ontherat@users.noreply.github.com>
2022-09-21 16:53:26 -0400

    Update sketch4.js
    Instead of a filled square, let's try a box of text.

--- sketch4.js
+++ sketch4.js
@@ -3,13 +3,14 @@ function setup() {
     cnv.position(50,200);
     x=0;
     y=0;  
-    
+    let s = 'ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quidem eveniet corporis minima id porro vero voluptate omnis. Neque fugit quidem dicta possimus error adipisci cum vitae sequi. Hic, maiores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque minima natus aspernatur numquam? Expedita doloremque consequatur libero. Odio quia repellat id esse error debitis mollitia, necessitatibus doloremque eaque, perferendis corrupti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis in molestiae dolorem laborum nostrum ullam consequatur nulla pariatur delectus doloribus fugiat, vel error explicabo cum beatae dolor facilis cumque aut.';
+
    }
      function draw() {
        background(51);
        angleMode(DEGREES); // Change the mode to DEGREES
        x=map(rotationY, -50, 50, 0, 900);
-       y=map(rotationX, 0, 90, 0, 1000);
+       y=map(rotationX, 0, 90, 0, 1200);
   /*     if (x > 275) {
          x=275;
         }if (y > 275) {
@@ -21,7 +22,8 @@ function setup() {
          y=0;
         }  */
       
-       rect(x,y,700,1000);
+      /* rect(x,y,700,1000); */
+       text(s, x, y, 700, 1000);  
      }
      function touchStarted() {
      noLoop();
